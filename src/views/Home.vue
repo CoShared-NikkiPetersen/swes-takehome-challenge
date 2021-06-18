94b93f64fdc200b9a3c4be345fcbf00a8c397a27
Nikki <piknik@me.com>
2021-06-18 15:38:01 -0600

    See all the data coming back successfully from db

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="home">
     <h3>Click on a link above, to see Companies or Buildings</h3>
+
+    <ul> Buildings
+      <li v-for="building in fetchedBuildings" :key="building.id">{{ building }}</li>
+    </ul>
+
+    <ul> Companies
+      <li v-for="company in fetchedCompanies" :key="company.id">{{ company }}</li>
+    </ul>
+
+    <ul> Employees
+      <li v-for="employee in fetchedEmployees" :key="employee.id">{{ employee }}</li>
+    </ul>
+
+    <ul> Offices
+      <li v-for="office in fetchedOffices" :key="office.id">{{ office }}</li>
+    </ul>
   </div>
 </template>
 
@@ -12,7 +28,10 @@ export default {
   name: 'Home',
   data () {
     return {
-      buildings: []
+      fetchedBuildings: [],
+      fetchedCompanies: [],
+      fetchedEmployees: [],
+      fetchedOffices: []
     }
   },
   components: {
@@ -24,7 +43,34 @@ export default {
         snapshot.forEach(doc => {
           const building = doc.data()
           building.id = doc.id
-          this.buildings.push(building)
+          this.fetchedBuildings.push(building)
+        })
+      })
+
+    db.collection('companies').get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          const company = doc.data()
+          company.id = doc.id
+          this.fetchedCompanies.push(company)
+        })
+      })
+
+    db.collection('employees').get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          const employee = doc.data()
+          employee.id = doc.id
+          this.fetchedEmployees.push(employee)
+        })
+      })
+
+    db.collection('offices').get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          const office = doc.data()
+          office.id = doc.id
+          this.fetchedOffices.push(office)
         })
       })
   }
