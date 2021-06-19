bea8455d473f3af2f207a453877a8415ed53da82
Nikki <piknik@me.com>
2021-06-19 07:38:27 -0600

    Move state management to store

--- src/views/Buildings.vue
+++ src/views/Buildings.vue
@@ -2,17 +2,61 @@
   <div class="buildings">
     <h1>Buildings</h1>
     <p>
-    <RouterLink :to="{ name: 'Building', params: {name: 'DEN1'}}">DEN1 </RouterLink>| companies in this building | total rent (based on occupancy) | available floors (1,3,6...)
+      <RouterLink :to="{ name: 'Building', params: {name: 'DEN1'}}">DEN1 </RouterLink>| companies in this building | total rent (based on occupancy) | available floors (1,3,6...)
     </p>
     <p>
       <RouterLink :to="{ name: 'Building', params: {name: 'CHI1'}}">CHI1 </RouterLink>| companies in this building | total rent (based on occupancy) | available floors (2,4...)
     </p>
+
+    <ul> Buildings
+      <li v-for="building in fetchedBuildings" :key="building.id">{{ building }}</li>
+    </ul>
+
+    <ul> Companies
+      <li v-for="company in fetchedCompanies" :key="company.id">{{ company }}</li>
+    </ul>
+
+    <ul> Employees
+      <li v-for="employee in fetchedEmployees" :key="employee.id">{{ employee }}</li>
+    </ul>
+
+    <ul> Offices
+      <li v-for="office in fetchedOffices" :key="office.id">{{ office }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
+
 export default {
-  name: 'Buildings'
+  name: 'Buildings',
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...mapState([
+      'fetchedBuildings',
+      'fetchedCompanies',
+      'fetchedEmployees',
+      'fetchedOffices'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'fetchBuildings',
+      'fetchCompanies',
+      'fetchEmployees',
+      'fetchOffices'
+    ])
+  },
+  created () {
+    this.fetchBuildings()
+    this.fetchCompanies()
+    this.fetchEmployees()
+    this.fetchOffices()
+  }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,78 +1,73 @@
 <template>
   <div class="home">
     <h3>Click on a link above, to see Companies or Buildings</h3>
-
-    <ul> Buildings
-      <li v-for="building in fetchedBuildings" :key="building.id">{{ building }}</li>
-    </ul>
-
-    <ul> Companies
-      <li v-for="company in fetchedCompanies" :key="company.id">{{ company }}</li>
-    </ul>
-
-    <ul> Employees
-      <li v-for="employee in fetchedEmployees" :key="employee.id">{{ employee }}</li>
-    </ul>
-
-    <ul> Offices
-      <li v-for="office in fetchedOffices" :key="office.id">{{ office }}</li>
-    </ul>
   </div>
 </template>
 
 <script>
-import firebase from '../firebaseConfig'
-const db = firebase
+// import { mapState, mapActions } from 'vuex'
+// import firebase from '../firebaseConfig'
+// const db = firebase
 
 export default {
   name: 'Home',
   data () {
     return {
-      fetchedBuildings: [],
-      fetchedCompanies: [],
-      fetchedEmployees: [],
-      fetchedOffices: []
+      // fetchedBuildings: [],
+      // fetchedCompanies: [],
+      // fetchedEmployees: [],
+      // fetchedOffices: []
     }
   },
   components: {
   },
+  computed: {
+    // ...mapState([
+    //   'fetchedBuildings'
+    // ])
+  },
+  methods: {
+    // ...mapActions([
+    //   'fetchBuildings'
+    // ])
+  },
   created () {
-    // todo ==> fetch to get data from back end
-    db.collection('buildings').get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          const building = doc.data()
-          building.id = doc.id
-          this.fetchedBuildings.push(building)
-        })
-      })
+    // this.fetchBuildings()
+    // db.collection('buildings').get()
+    //   .then(snapshot => {
+    //     snapshot.forEach(doc => {
+    //       const building = doc.data()
+    //       building.id = doc.id
+    //       this.fetchedBuildings.push(building)
+    //     })
+    //   })
 
-    db.collection('companies').get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          const company = doc.data()
-          company.id = doc.id
-          this.fetchedCompanies.push(company)
-        })
-      })
-
-    db.collection('employees').get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          const employee = doc.data()
-          employee.id = doc.id
-          this.fetchedEmployees.push(employee)
-        })
-      })
-
-    db.collection('offices').get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          const office = doc.data()
-          office.id = doc.id
-          this.fetchedOffices.push(office)
-        })
-      })
+    // db.collection('companies').get()
+    //   .then(snapshot => {
+    //     snapshot.forEach(doc => {
+    //       const company = doc.data()
+    //       company.id = doc.id
+    //       this.fetchedCompanies.push(company)
+    //     })
+    //   })
+    //
+    // db.collection('employees').get()
+    //   .then(snapshot => {
+    //     snapshot.forEach(doc => {
+    //       const employee = doc.data()
+    //       employee.id = doc.id
+    //       this.fetchedEmployees.push(employee)
+    //     })
+    //   })
+    //
+    // db.collection('offices').get()
+    //   .then(snapshot => {
+    //     snapshot.forEach(doc => {
+    //       const office = doc.data()
+    //       office.id = doc.id
+    //       this.fetchedOffices.push(office)
+    //     })
+    //   })
   }
 }
 </script>
