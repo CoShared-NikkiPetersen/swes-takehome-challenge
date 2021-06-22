c6ddac09dad9bf15d1fb075759b3d4f77b7d1df8
Nikki <piknik@me.com>
2021-06-22 05:06:22 -0600

    Get employee count right

--- src/components/Building.vue
+++ src/components/Building.vue
@@ -11,7 +11,7 @@
       </ul>
     <h3>Total income based on current occupancy:  {{ calculateIncome() }}</h3>
     <h3>Available floors: <span>{{ vacancies.join(', ') }} </span></h3>
-    <h3>Employees in this building:  {{ fetchedEmployees }}</h3>
+    <h3>Employees in this building:  {{ currentEmployees }}</h3>
   </div>
 </template>
 
@@ -47,8 +47,16 @@ export default {
       return floors.filter(floor => !occ.includes(floor))
     },
     currentEmployees: function () {
-      const occupied = this.fetchedOffices.filter(office => office.building === this.building.name)
-      return this.fetchedEmployees.filter(employee => occupied.includes(employee.company))
+      // todo ==> figure out why this filter won't get just the company name
+      const tenants = []
+      const occupants = this.fetchedOffices.filter(office => {
+        if (office.building === this.building.name) {
+          tenants.push(office.company)
+          return office.company
+        }
+      })
+      console.log(occupants)
+      return this.fetchedEmployees.filter(employee => tenants.includes(employee.company)).length
     }
   },
   methods: {
@@ -58,6 +66,7 @@ export default {
     }
     // findEmployees: function () {
     //   const inBuilding = this.fetchedOffices.filter(office => office.building === this.building.name)
+    //   console.log(inBuilding)
     //   return this.fetchedEmployees.filter(employee => inBuilding.includes(employee.company))
     // }
     // displayOccupied: function () {
