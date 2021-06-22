d483490f6f690a42d217f9e5908126f7c2383543
Nikki <piknik@me.com>
2021-06-22 06:18:33 -0600

    Basic form there need to come back to this

--- src/components/AddCompany.vue
+++ src/components/AddCompany.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="form-container">
     <form class="form" @submit.prevent="AddNew">
-      Add a new tenant
+<!--      <label for="companyFloor">Choose an available floor: </label>-->
+<!--      <select v-model="selected"  id="companyFloor" required>-->
+<!--        <option v-for="floor in vacancies" :value="floor" :key="floor">{{ floor }}</option>-->
+<!--      </select>-->
+
       <label for="companyName">Company Name: </label>
       <input id="companyName" type="text" name="companyName" minlength="5" v-model="companyName" required>
       <button class="button">Submit</button>
@@ -13,12 +17,35 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'AddCompany',
   data () {
     return {
-      companyName: ''
+      companyName: '',
+      selected: ''
     }
+  },
+  computed: {
+    ...mapState([
+      'fetchedBuildings',
+      'fetchedCompanies',
+      'fetchedEmployees',
+      'fetchedOffices'
+    ]),
+    vacancies: function () {
+      const occ = this.fetchedOffices.filter(office => office.building === this.building.name).map(office => office.floor).sort()
+      const floors = []
+      for (let i = 1; i < this.building.number_of_floors + 1; i++) {
+        floors.push(i)
+      }
+      return floors.filter(floor => !occ.includes(floor))
+    }
+  },
+  methods: {
+    // when adding a company, it gets added to the building and to offices
+    // company should also have employees
   }
 }
 </script>
--- src/views/Companies.vue
+++ src/views/Companies.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="companies">
-    <h1>Companies</h1>
-    <button @click="showForm = !showForm">Add a new tenant</button>
+    <div class="row-container">
+      <h1>Companies</h1>
+      <button @click="showForm = !showForm">Add a new tenant</button>
+    </div>
+
     <AddCompany v-show="showForm" />
-    <p>
-      <RouterLink :to="{ name: 'Company', params: {name: 'Schmoogle'}}" >Schmoogle </RouterLink>
-    </p>
-    <p>
-      <RouterLink :to="{ name: 'Company', params: {name: 'Bloogle'}}" >Bloogle</RouterLink>
-    </p>
-    <p>
-      <RouterLink :to="{ name: 'Company', params: {name: 'Zazamon'}}" >Zazamon</RouterLink>
-    </p>
-    <hr>
-    <h3>Add a new company (form)</h3>
+    <ul>
+      <li v-for="company in fetchedCompanies" :key="company.id">
+        <RouterLink
+          :to="{ name: 'Company', params: { name: company.name, id: company.id }}"
+        >
+          {{ company.name }}
+        </RouterLink>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import AddCompany from '../components/AddCompany'
+import { mapState } from 'vuex'
 
 export default {
   name: 'Companies',
@@ -27,10 +29,26 @@ export default {
       showForm: false
     }
   },
-  components: { AddCompany }
+  components: { AddCompany },
+  computed: {
+    ...mapState([
+      'fetchedBuildings',
+      'fetchedCompanies',
+      'fetchedEmployees',
+      'fetchedOffices'
+    ])
+  }
 }
 </script>
 
 <style scoped>
+.row-container {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
 
+button {
+  margin: 0 20px;
+}
 </style>
